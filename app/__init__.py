e44014d003381d985641a834b4c3c42545b15b0a
jararasifdev <jarar.asif@devsinc.com>
2026-05-21 18:29:26 +0500

    rigestered blueprints for dashboard,employee and departments routes

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,14 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
 
     from app.auth.routes import auth_bp
+    from app.dashboard.routes import dashboard_bp
+    from app.employees.routes import employees_bp
+    from app.departments.routes import departments_bp
 
+
+    app.register_blueprint(dashboard_bp)
+    app.register_blueprint(employees_bp)
+    app.register_blueprint(departments_bp)
     app.register_blueprint(auth_bp)
 
     return app
